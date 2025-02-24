836be9fcff325a4f8e1306451b9e8a65483af0c8
Ellie Solhjou <elliesolhjou@gmail.com>
2025-02-24 15:16:58 -0800

    readInput is set

--- BookshelfKeeperProg.java
+++ BookshelfKeeperProg.java
@@ -3,6 +3,32 @@ import java.util.Scanner;
 
 public class BookshelfKeeperProg{
     public static void main (String[] args){
-        
+        Scanner in = new Scanner();
+
+        // String input = int.NextLine();
+        // System.out.print(input.toString());
     }
 }
+
+
+
+private static ArrayList<Integer> readInput(Scanner in){
+    ArrayList<Integer> initialBookList = new ArrayList<Integer>();
+    System.out.println("Please enter initial arrangement of books followed by newline:");
+    String initialLine = in.nextLine();
+    Scanner lineScanner = new Scanner(initialLine);
+    while (lineScanner.hasNext()){
+        if (lineScanner.hasNextInt()){
+            initialBookList.add(lineScanner.nextInt());
+        }
+        else{
+            lineScanner.next();
+        }
+        return initialBookList;
+    }
+
+}
+
+private static void validateInput(){}
+
+private static void runcCommands(){}
